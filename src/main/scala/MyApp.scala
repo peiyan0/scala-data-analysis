34cac01ea32841b92c3b55fc3bd1f656889a1ee1
peiyan0 <peiyan0802@gmail.com>
2024-07-03 17:28:12 +0800

    initialize csv file, read data from csv using scala.io.Source

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -1,3 +1,13 @@
+import scala.io.Source
+import scala.collection._
+
 object MyApp extends App {
-  println("hello world")
+  val path = "src/main/resources/aefi.csv"
+  val source = Source.fromFile(path)
+  val lines = source.getLines()
+
+  // read data from csv
+  lines.foreach(println)
+
+  source.close()
 }
