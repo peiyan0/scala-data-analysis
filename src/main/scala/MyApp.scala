0515e0d1c0c72655c6f2b71c28518b30910d86e2
peiyan0 <peiyan0802@gmail.com>
2024-07-09 09:45:48 +0800

    case class created

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -1,13 +1,20 @@
 import scala.io.Source
 import scala.collection._
 
+case class AEFIData(
+                     date: String,
+                     vaxtype: String,
+                     d1_headache: Int,
+                     d2_headache: Int,
+                     d1_vomiting: Int,
+                     d2_vomiting: Int
+                   )
+
 object MyApp extends App {
   val path = "src/main/resources/aefi.csv"
   val source = Source.fromFile(path)
   val lines = source.getLines()
 
-  // read data from csv
-  lines.foreach(println)
 
   source.close()
 }
