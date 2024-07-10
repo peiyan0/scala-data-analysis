701cca74eb3c85002e2400ff52a55e8a1cade086
peiyan0 <peiyan0802@gmail.com>
2024-07-10 12:50:12 +0800

    Reorganize code for better readability and structure
    - adjusted the sequence of function definitions and vals for a more logical flow
    - removed param for defs used for q1,2,3
    - removed Date, d2_vomiting from case class AEFIData
    - added more comments for defs
    - ensured that code is more readable
    - organized console output
    - fixed indentation

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -3,35 +3,29 @@ import scala.collection._
 
 // extract useful data
 case class AEFIData(
-                     date: String,
                      vaxtype: String,
                      d1_headache: Int,
                      d2_headache: Int,
                      d1_vomiting: Int,
-                     d2_vomiting: Int
                    )
 
 object MyApp extends App {
+  // location of aefi.csv
   val path = "src/main/resources/aefi.csv"
-
-  // read data
+  // read data from aefi.csv
   val aefiData = readAEFIData(path)
-  // data group by vax type
+  // group aefiData by vax type
   val aefiGroupedData = aefiData.groupBy(_.vaxtype)
-  val totalDosesByVaxType = getTotalDosesByVaxType(aefiData)
+  val totalDosesByVaxType = getTotalDosesByVaxType()
   val mostCommonVaccine = totalDosesByVaxType.maxBy(_._2)
-  // Calculate average occurrences of headache for each vaccination type
-  val avgHeadacheByVaxType = getAverageHeadacheByVaxType(aefiData)
-  val vomitingAfterFirstInjection = getVomitingAfterFirstInjection(aefiData)
+  val avgHeadacheByVaxType = getAverageHeadacheByVaxType()
+  val vomitingAfterFirstInjection = getVomitingAfterFirstInjection()
 
 
   // question 1
+  println("PRG2103 Assignment 2")
   println("\nQuestion 1: Which vaccination product is the most commonly used by Malaysian?")
-  println("Total doses by vaccine type:")
   val highestVomitingVaxType = vomitingAfterFirstInjection.maxBy(_._2)
-  totalDosesByVaxType.foreach { case (vaxType, totalDoses) =>
-    println(s"$vaxType: $totalDoses")
-  }
 
   def readAEFIData(filePath: String): List[AEFIData] = {
     val source = Source.fromFile(filePath)
@@ -39,19 +33,21 @@ object MyApp extends App {
     val data = lines.map { line =>
       val cols = line.split(",").map(_.trim)
       AEFIData(
-        cols(0),
         // vaccination type
         cols(1),
         // headache
-        cols(12).toInt,
-        cols(24).toInt,
+        cols(12).toInt, // d1_headache
+        cols(24).toInt, // d2_headache
         // vomiting
-        cols(17).toInt,
-        cols(29).toInt
+        cols(17).toInt, // d1_vomiting
       )
     }.toList
     source.close()
     data
+  }
+  println("Total doses for each vaccine type:")
+  totalDosesByVaxType.foreach { case (vaxType, totalDoses) =>
+    println(s"$vaxType: $totalDoses")
   }
   println(s"Ans: The most commonly used vaccine is ${mostCommonVaccine._1} with a total of ${mostCommonVaccine._2} doses.")
 
@@ -60,12 +56,12 @@ object MyApp extends App {
   println("\nQuestion 2: What are the average occurrence of headache for each type of vaccination product in the provided data?")
 
   // function used in question 1
-  def getTotalDosesByVaxType(data: List[AEFIData]): Map[String, Int] = {
+  // get total number of doses by vax type
+  def getTotalDosesByVaxType(): Map[String, Int] = {
     aefiGroupedData.map { case (vaxType, records) =>
       vaxType -> records.size // total doses
     }
   }
-  // display each brand
   println("Ans: \nAverage occurrence of headache for each vaccination product:")
   avgHeadacheByVaxType.foreach { case (vaxType, avgHeadache) =>
     println(s"$vaxType: $avgHeadache")
@@ -73,28 +69,28 @@ object MyApp extends App {
 
 
   // question 3
-  // Identify the vaccination type with the highest occurrence of vomiting after the first injection
   println("\nQuestion 3: Which vaccination type has the highest occurrence of vomiting after first injection in the provided data?")
 
   // function used in question 2
-  def getAverageHeadacheByVaxType(data: List[AEFIData]): Map[String, Double] = {
+  // get average headache by vax type
+  def getAverageHeadacheByVaxType(): Map[String, Double] = {
     aefiGroupedData.map { case (vaxType, records) =>
       val totalHeadache = records.map(r => r.d1_headache + r.d2_headache).sum
       vaxType -> (totalHeadache.toDouble / records.size) // average headache
     }
   }
-  // display each brand
-  println("Total vomiting occurrences after first injection by type:")
-  vomitingAfterFirstInjection.foreach { case (vaxType, totalVomitingD1) =>
-    println(s"$vaxType: $totalVomitingD1")
-  }
 
   // function used in question 3
-  def getVomitingAfterFirstInjection(data: List[AEFIData]): Map[String, Int] = {
+  // get vomiting occurrence after first injection by vax type
+  def getVomitingAfterFirstInjection(): Map[String, Int] = {
     aefiGroupedData.map { case (vaxType, records) =>
       val totalVomitingD1 = records.map(_.d1_vomiting).sum
       vaxType -> totalVomitingD1 // total vomiting after first injection
     }
+  }
+  println("Total vomiting occurrences after first injection by type:")
+  vomitingAfterFirstInjection.foreach { case (vaxType, totalVomitingD1) =>
+    println(s"$vaxType: $totalVomitingD1")
   }
   println(s"Ans: The vaccination type with the highest occurrence of vomiting after the first injection is ${highestVomitingVaxType._1} with ${highestVomitingVaxType._2} occurrences.")
 }
