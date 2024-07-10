1eeadf4c9971d94a7a3c3c519ca481710b1bfb83
peiyan0 <peiyan0802@gmail.com>
2024-07-10 13:02:50 +0800

    Refactor code - adjusted the sequence of function definitions and vals for a more logical flow - used same variable name across the program

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -10,23 +10,6 @@ case class AEFIData(
                    )
 
 object MyApp extends App {
-  // location of aefi.csv
-  val path = "src/main/resources/aefi.csv"
-  // read data from aefi.csv
-  val aefiData = readAEFIData(path)
-  // group aefiData by vax type
-  val aefiGroupedData = aefiData.groupBy(_.vaxtype)
-  val totalDosesByVaxType = getTotalDosesByVaxType()
-  val mostCommonVaccine = totalDosesByVaxType.maxBy(_._2)
-  val avgHeadacheByVaxType = getAverageHeadacheByVaxType()
-  val vomitingAfterFirstInjection = getVomitingAfterFirstInjection()
-
-
-  // question 1
-  println("PRG2103 Assignment 2")
-  println("\nQuestion 1: Which vaccination product is the most commonly used by Malaysian?")
-  val highestVomitingVaxType = vomitingAfterFirstInjection.maxBy(_._2)
-
   def readAEFIData(filePath: String): List[AEFIData] = {
     val source = Source.fromFile(filePath)
     val lines = source.getLines().drop(1) // Skip header line
@@ -45,15 +28,13 @@ object MyApp extends App {
     source.close()
     data
   }
-  println("Total doses for each vaccine type:")
-  totalDosesByVaxType.foreach { case (vaxType, totalDoses) =>
-    println(s"$vaxType: $totalDoses")
-  }
-  println(s"Ans: The most commonly used vaccine is ${mostCommonVaccine._1} with a total of ${mostCommonVaccine._2} doses.")
+  // location of aefi.csv
+  val path = "src/main/resources/aefi.csv"
+  // read data from aefi.csv
+  val aefiData = readAEFIData(path)
+  // group aefiData by vax type
+  val aefiGroupedData = aefiData.groupBy(_.vaxtype)
 
-
-  // question 2
-  println("\nQuestion 2: What are the average occurrence of headache for each type of vaccination product in the provided data?")
 
   // function used in question 1
   // get total number of doses by vax type
@@ -62,15 +43,6 @@ object MyApp extends App {
       vaxType -> records.size // total doses
     }
   }
-  println("Ans: \nAverage occurrence of headache for each vaccination product:")
-  avgHeadacheByVaxType.foreach { case (vaxType, avgHeadache) =>
-    println(s"$vaxType: $avgHeadache")
-  }
-
-
-  // question 3
-  println("\nQuestion 3: Which vaccination type has the highest occurrence of vomiting after first injection in the provided data?")
-
   // function used in question 2
   // get average headache by vax type
   def getAverageHeadacheByVaxType(): Map[String, Double] = {
@@ -79,7 +51,6 @@ object MyApp extends App {
       vaxType -> (totalHeadache.toDouble / records.size) // average headache
     }
   }
-
   // function used in question 3
   // get vomiting occurrence after first injection by vax type
   def getVomitingAfterFirstInjection(): Map[String, Int] = {
@@ -88,7 +59,33 @@ object MyApp extends App {
       vaxType -> totalVomitingD1 // total vomiting after first injection
     }
   }
-  println("Total vomiting occurrences after first injection by type:")
+
+  println("PRG2103 Assignment 2")
+  // question 1
+  println("\nQuestion 1: Which vaccination product is the most commonly used by Malaysian?")
+  val totalDosesByVaxType = getTotalDosesByVaxType()
+  val mostCommonVaccine = totalDosesByVaxType.maxBy(_._2)
+  println("Total doses for each vaccine product:")
+  totalDosesByVaxType.foreach { case (vaxType, totalDoses) =>
+    println(s"$vaxType: $totalDoses")
+  }
+  println(s"Ans: The most commonly used vaccine product is ${mostCommonVaccine._1} with a total of ${mostCommonVaccine._2} doses.")
+
+
+  // question 2
+  println("\nQuestion 2: What are the average occurrence of headache for each type of vaccination product in the provided data?")
+  val avgHeadacheByVaxType = getAverageHeadacheByVaxType()
+  println("Ans: \nAverage occurrence of headache for each type of vaccination product:")
+  avgHeadacheByVaxType.foreach { case (vaxType, avgHeadache) =>
+    println(s"$vaxType: $avgHeadache")
+  }
+
+
+  // question 3
+  println("\nQuestion 3: Which vaccination type has the highest occurrence of vomiting after first injection in the provided data?")
+  val vomitingAfterFirstInjection = getVomitingAfterFirstInjection()
+  val highestVomitingVaxType = vomitingAfterFirstInjection.maxBy(_._2)
+  println("Total vomiting occurrences after first injection by vaccination type:")
   vomitingAfterFirstInjection.foreach { case (vaxType, totalVomitingD1) =>
     println(s"$vaxType: $totalVomitingD1")
   }
